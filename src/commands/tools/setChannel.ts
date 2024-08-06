71631949b41453ac98263b80a651bec4f57035ff
水原千鶴 <121162902+archue001@users.noreply.github.com>
2024-08-06 17:46:55 +0800

    v2.3.0 add the option to choise channel

---
+++ src/commands/tools/setChannel.ts
@@ -0,0 +1,49 @@
+import { readFileSync, writeFileSync } from "fs";
+import { ExtendedClient } from "../../types/ExtendedClient";
+import { ChannelType, ChatInputCommandInteraction, SlashCommandBuilder, TextChannel } from "discord.js";
+import { local_subscribe } from "../../types/subData";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName(`set_channel`)
+    .setNameLocalization("zh-TW", "設定通知頻道")
+    .setDescription(`設置漫畫通知的頻道`)
+    .addStringOption((option) =>
+      option
+        .setName(`option`)
+        .setDescription(`設定要移動的項目`)
+        .setChoices(
+          {
+            name: `hanime1`,
+            value: `hanime1`,
+          },
+          {
+            name: `manhuagui`,
+            value: `manhuagui`,
+          }
+        )
+        .setRequired(true)
+    )
+    .addChannelOption((optoin) => optoin.setName(`channel`).setDescription(`要移去的頻道`).setRequired(true)),
+
+  async execute(interaction: ChatInputCommandInteraction, client: ExtendedClient) {
+    const option = interaction.options.getString(`option`);
+    const channel = interaction.options.getChannel(`channel`);
+    if (!(channel?.type === ChannelType.GuildText)) {
+      await interaction.reply({
+        content: `選擇到非法頻道`,
+        ephemeral: true,
+      });
+      return;
+    }
+    const filePath = `./resource/${option}/${interaction.guildId}.json`;
+    const localData: local_subscribe = JSON.parse(readFileSync(filePath, "utf-8"));
+    console.debug(filePath);
+    localData.channel = channel.id;
+    writeFileSync(filePath, JSON.stringify(localData, null, 2), "utf-8");
+    await interaction.reply({
+      content: `已經將${option}移至${channel.name}`,
+      ephemeral: true,
+    });
+  },
+};
