9e3ea91340a04f6ff802d101a93367ae0734a145
archie0732 <killer.archie.0732@gmail.com>
2024-08-09 14:40:32 +0800

    formation & h1 local data add video url

--- src/api/hanime1/hanime1_a.ts
+++ src/api/hanime1/hanime1_a.ts
@@ -16,17 +16,22 @@ export async function hanime1A(client: ExtendedClient) {
   for (const file of folder) {
     const filePath = join(folderPath, file);
 
-    const localData: local_subscribe = JSON.parse(readFileSync(filePath, "utf-8"));
+    const localData: local_subscribe = JSON.parse(
+      readFileSync(filePath, "utf-8")
+    );
     const channel = await client.channels.fetch(localData.channel);
     let flag: boolean = false;
     for (const entry of localData.sub) {
       try {
         const hanime1 = await hanime1_fetch(entry.name!);
         if (hanime1.id !== entry.id) {
-          console.log(chalk.yellow(`[hanime1]${hanime1.tag} - 更新了: ${hanime1.title}`));
+          console.log(
+            chalk.yellow(`[hanime1]${hanime1.tag} - 更新了: ${hanime1.title}`)
+          );
           await sendMessage(client, channel!, hanime1);
           entry.last_up = hanime1.title;
           entry.id = hanime1.id;
+          entry.other = hanime1.video_url;
           flag = true;
         }
       } catch (error) {
@@ -41,20 +46,30 @@ export async function hanime1A(client: ExtendedClient) {
   }
 }
 
-async function sendMessage(client: ExtendedClient, channel: Channel, hanime1: Hanime1) {
+async function sendMessage(
+  client: ExtendedClient,
+  channel: Channel,
+  hanime1: Hanime1
+) {
   const embed = new EmbedBuilder()
     .setAuthor({
       name: `${client.user?.tag} - 被乙骨操作的機器人`,
       iconURL: client.user?.displayAvatarURL(),
     })
-    .setTitle(`[hanime1]您訂閱的 ${hanime1.tag} - 更新了新的內容: ${hanime1.title}`)
+    .setTitle(
+      `[hanime1]您訂閱的 ${hanime1.tag} - 更新了新的內容: ${hanime1.title}`
+    )
     .setURL(hanime1.video_url)
     .setDescription(
       `- 使用 </sub_hanime1:1268195537287381024> 訂閱更多作者或標籤\n- 或是用 </rm_hanime1:1268195537287381023> 來取消訂閱`
     )
     .setThumbnail(`https://www.iconsdb.com/icons/preview/red/letter-h-xxl.png`)
     .addFields(
-      { name: `🛜 原網站`, value: `[hanime](https://hanime1.me/)`, inline: true },
+      {
+        name: `🛜 原網站`,
+        value: `[hanime](https://hanime1.me/)`,
+        inline: true,
+      },
       {
         name: `🔍 作者 / 發行商`,
         value: `${hanime1.artist}`,
@@ -74,7 +89,9 @@ async function sendMessage(client: ExtendedClient, channel: Channel, hanime1: Ha
   try {
     if (channel && channel.isTextBased()) {
       await channel.send({
-        content: `您在 [hanime1] 訂閱的 [${hanime1.tag}](https://hanime1.me/search?query=${hanime1.tag.replace(
+        content: `您在 [hanime1] 訂閱的 [${
+          hanime1.tag
+        }](https://hanime1.me/search?query=${hanime1.tag.replace(
           / /g,
           "%20"
         )}) 更新了 [${hanime1.title}](${hanime1.video_url})`,
